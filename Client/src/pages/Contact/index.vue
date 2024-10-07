d962aa83819875a6e36cb19b316eac46e6472c30
mudasirkk <mudasirkkhan@gmail.com>
2024-10-07 15:11:54 -0400

    File based routing
    Renaming view folder as pages folder
    Adding Jobs and Contact pages

---
+++ Client/src/pages/Contact/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="title">Contact Us</h1>
+    <h2 class="subtitle">We'd love to hear from you</h2>
+
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" /><br /><br />
+      <label for="email">Email:</label>
+      <input type="text" id="email" name="email" /><br /><br />
+      <label for="message">Message:</label><br />
+      <textarea id="message" name="message" rows="4" cols="50"></textarea><br /><br />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ Client/src/pages/Jobs.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="title">The Jobs Pages</h1>
+    <h2 class="subtitle">This is an example of a jobs page</h2>
+    <p>Here is a list of jobs</p>
+  </div>
+</template>
+
+<style scoped></style>
