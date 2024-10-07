f3c709d756b791e1a98c0639dc366449ccbe7038
mudasirkk <mudasirkkhan@gmail.com>
2024-10-07 14:10:51 -0400

    Updated Router and Header

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
+    <NavBar />
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
---
+++ Client/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <img alt="Vue logo" src="@/assets/logo.svg" width="30" height="30" />
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <RouterLink to="/home" class="navbar-item"> Home </RouterLink>
+        <RouterLink to="/about" class="navbar-item"> About </RouterLink>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light"> Log in </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
