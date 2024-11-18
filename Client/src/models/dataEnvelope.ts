2964db221d7c1397d1ed9eb2d38f7643c8fe2663
mudasirkk <mudasirkkhan@gmail.com>
2024-11-18 15:19:34 -0500

    Updating Models and Pages for Async

--- Client/src/models/dataEnvelope.ts
+++ Client/src/models/dataEnvelope.ts
@@ -1,10 +1,10 @@
 export interface DataEnvelope<T> {
   data: T
-  error?: string
+  message?: string
+  isSuccess: boolean
 }
 
-export interface DataListEnvelope<T> {
+export interface DataListEnvelope<T> extends DataEnvelope<T[]> {
   data: T[]
   total: number
-  error?: string
 }
