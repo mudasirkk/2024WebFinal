e7fcdec2244fc18dff989ce1c3630665e52ba71f
mudasirkk <mudasirkkhan@gmail.com>
2024-12-09 14:49:09 -0500

    Got rid of boilerplate

---
+++ react/src/App.tsx
@@ -0,0 +1,34 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const NavBar = () => <nav>NavBar</nav>;
+
+const Home = () => (
+  <div>
+    <h1 className="title">Home Page</h1>
+    <h2 className="subtitle">This is our home page</h2>
+  </div>
+);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <header>
+        <NavBar />
+      </header>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react/src/main.tsx
@@ -0,0 +1,12 @@
+import "bulma/css/bulma.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
